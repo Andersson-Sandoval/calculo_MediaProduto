3cb3b447a5b3f8510d6143827f3b950203a7ca47
Andersson-Sandoval <andersson.sval@hotmail.com>
2022-06-19 16:04:22 -0300

    Adicionar arquivos de projeto.

---
+++ calculo_mediaAltura/Product.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace calculo_mediaAltura
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ calculo_mediaAltura/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+namespace calculo_mediaAltura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos Produtos deseja calcular a Média? ");
+          
+            
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] vect = new Product[n];
+
+            Console.WriteLine("Digite o Nome e valor dos elementos:");
+
+            for( int i = 0; i < n; i++ ) {
+
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine());
+
+
+                vect[i] = new Product(name, price);
+            
+            }
+
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += vect[i].Price;
+
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine("A Média dos elementos é " + avg.ToString("F2", CultureInfo.InvariantCulture)) ; 
+
+
+        }
+    }
+}
